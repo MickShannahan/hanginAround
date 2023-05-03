7a8dc888f808a937c4637372b44e752ee9148618
mickshannahan <beasticorn@gmail.com>
2023-05-03 15:41:40 -0600

    build

--- hanginAround.client/src/env.js
+++ hanginAround.client/src/env.js
@@ -1,5 +1,5 @@
-export const dev = window.location.includes('localhost')
-export const baseURL = dev ? 'http://192.168.1.159:3000' : ''
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'mickshanny.us.auth0.com'
 export const clientId = 'luwNBP378EfKtXGGgxJj9T2kNZ1A562I'
